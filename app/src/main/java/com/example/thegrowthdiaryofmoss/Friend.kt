867dfd73becde6a10f6bd786eaca834785f89143
Resfeber <69678585+Resfebel@users.noreply.github.com>
2023-07-31 23:41:20 +0900

    one
    one

--- app/src/main/java/com/example/thegrowthdiaryofmoss/Friend.kt
+++
@@ -1,3 +0,0 @@
-package com.example.thegrowthdiaryofmoss
-
-data class Friend(val id: String, val name: String, val nickname: String)
--- app/src/main/java/com/example/thegrowthdiaryofmoss/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.example.thegrowthdiaryofmoss
-
-data class Message(
-    val senderId: String,
-    val senderNickname: String,
-    val content: String,
-    val timestamp: Long
-)
