2b4515ac43b5dc34c2d8d691b7dd78ca0f6f007f
Resfeber <69678585+Resfebel@users.noreply.github.com>
2023-07-31 23:47:19 +0900

    two
    two

--- app/src/main/java/com/example/guru2_project_25/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.example.guru2_project_25
-
-data class Message(
-    val senderId: String,
-    val senderNickname: String,
-    val content: String,
-    val timestamp: Long
-)
--- src/main/java/com/example/guru2_project_25/ClosetItemSelectionListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.guru2_project_25
-
-interface ClosetItemSelectionListener {
-    fun onClosetItemSelectionConfirmed(itemImageResource: Int){
-
-    }
-}
